777b063c60b945ff55451ea4dbea780703077aac
Marko Milenovic <markomilenovic7@gmail.com>
2023-01-11 14:55:24 +0100

    Napravljena aplikacija c koja stampa

---
+++ Folder/app.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+
+    printf("Avione slomicu ti krila.");
+
+
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## TO-DO :
     Treba odraditi prvo ovo.
+    Napraviti C program koji stampa naziv aplikacije.
